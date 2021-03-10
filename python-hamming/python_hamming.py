d437a2a2fc6ccccc0ad0875f2bb7e64de6697f8a
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-03-10 14:54:24 +0100

    first commit

---
+++ python-hamming/python_hamming.py
@@ -0,0 +1,27 @@
+# get the hamming distancs between two strands of DNA
+# if DNA strands have equal length
+    # count different nucleotides
+    # print the result
+
+string_a = 'GAGCCTACTAACGGGAT'
+string_b = 'CATCGTAATGACGGCCT'
+
+
+def split_strings(str_a, str_b):
+    strand_a = str_a.split
+    strand_b = str_b.split
+    strands = [strand_a, strand_b]
+    print(strands)
+    return strands
+
+
+def count_different_nucleotides():
+    split_strings(string_a, string_b)
+
+    if(strands[0].length == strands[0].length):
+        print('yes')
+    else:
+        print('DNA strands of different length cant\'t be used!')
+
+
+count_different_nucleotides()
