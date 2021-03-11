f3f022f8195497fb7c5ca189b9fdf84f6f4e6c95
AuroreVanHoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-03-11 09:29:12 +0100

    refactor for shorter and simpler answer

--- python-hamming/hamming.py
+++ python-hamming/hamming.py
@@ -1,35 +1,13 @@
+def distance(string_a: str, string_b: str):
+    if len(string_a) == len(string_b):
+        count = 0
 
-# get the hamming distance between two strands of DNA
-# if DNA strands have equal length
-    # count different nucleotides
-    # print the result
+        for i in range(0, len(string_a), 1):
 
-string_a = 'GAGCCTACTAACGGGAT'
-string_b = 'CATCGTAATGACGGCCT'
+            if string_a[i] != string_b[i]:
+                count += 1
+        return count
 
-
-def map_strings(str_a, str_b):
-    d = dict();
-    d['strand_a'] = list(map(str, str_a))
-    d['strand_b'] = list(map(str, str_b))
-    return d
-
-def get_difference(strand_a, strand_b):
-    print(strand_a)
-    print(strand_b)
-    return sum(i != j for i, j in zip(strand_a, strand_b))
-
-def count_different_nucleotides():
-    strands = map_strings(string_a, string_b)
-    print(strands)
-    strand_a = strands['strand_a']
-    strand_b = strands['strand_b']
-
-    if len(strand_a) == len(strand_b):
-        print('yes')
-        result = get_difference(strand_a, strand_b)
-        print(result)
     else:
         print('DNA strands of different length cant\'t be used!')
-
-count_different_nucleotides()
+        return -1
