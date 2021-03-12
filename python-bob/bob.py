e36183572e8248b0e14b5b7d36d78d01465c7999
AuroreVanHoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-03-12 16:52:55 +0100

    solved challenge bob

--- python-bob/bob.py
+++ python-bob/bob.py
@@ -1,8 +1,26 @@
 #
 # Skeleton file for the Python "Bob" exercise.
 #
+# Bob answers 'Sure.' if you ask him a question.
+# He answers 'Whoa, chill out!' if you yell at him.
+# He says 'Fine. Be that way!' if you address him without actually saying anything.
+# He answers 'Whatever.' to anything else.
 
 
-def hey(what):
+def hey(what: str):
+    what = what.strip()
 
-    return
+    if not what or what.isspace():
+        return 'Fine. Be that way!'
+
+    if what.endswith('?'):
+        if what.isupper():
+            pass
+        else:
+            return 'Sure.'
+
+    if what.isupper():
+        return 'Whoa, chill out!'
+
+    else:
+        return 'Whatever.'
