dec4a0720b86af931fdd0a8ce89f8b5c19f1dddb
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-03-15 15:48:57 +0100

    End function decode (pass small words and uppercase tests)

--- python-atbash-cipher/atbash_cipher.py
+++ python-atbash-cipher/atbash_cipher.py
@@ -2,31 +2,23 @@
 # encoding => comparing input to alphabet list and find indexes of used letters
 # store indexes used
 # compare to reversed list. use the letters at stored index (reversed alphabet letter)
-
-
-
-
-string = 'no'
-
+#
+# string = 'no'
+#
 
 def encode(input: str):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     reversed_alphabet = alphabet[::-1]
-
-    print('reversed_aphabet: ', reversed_alphabet)
+    input = input.lower()
     print('input: ', input)
-    # print('input len: ', len(input))
-    print(alphabet)
-    # print('alphabet len: ', len(alphabet))
+    print('input len: ', len(input))
+
     result = []
 
     for i in range(len(input)):
-        # print('In i loop', i, alphabet[i])
 
-        print('i: ', i)
-        print('alphabet.find: ', alphabet.find(input[i]))
         found_index = alphabet.find(input[i])
-        print('found_index: ', found_index)
+
         result.append(found_index)
 
     print('result: ', result)
@@ -35,29 +27,30 @@ def encode(input: str):
 
     for j in range(len(result)):
         encoded_string = encoded_string + reversed_alphabet[result[j]]
-        print('encoded_string: ', encoded_string)
 
-    print('oput loop j: ', encoded_string)
+    print('encoded_string: ', encoded_string)
+
     return encoded_string
 
+
 def decode():
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    reversed_alphabet = alphabet[::-1]
+
     encoded_string = encode(string)
     print('///////////////////////////')
     print('in decode function - result: ', encoded_string)
 
+    result = []
 
+    for i in range(len(encoded_string)):
+        index_in_encoded = reversed_alphabet.find(encoded_string[i])
+        result.append(index_in_encoded)
+    print(result)
 
+    decoded_string = ''
 
+    for j in range(len(result)):
+        decoded_string = decoded_string + alphabet[result[j]]
 
-    #
-    #     # for j in range(len(alphabet_list), 0, 1):
-    #     #     encoded_alphabet = []
-    #     #     print('in j loop before permutation', encoded_alphabet)
-    #     #
-    #     #     encoded_alphabet[j] = alphabet_list[i]
-    #     #
-    #     #     print(alphabet_list)
-
-
-# encode(string)
-decode()
+    return decoded_string
